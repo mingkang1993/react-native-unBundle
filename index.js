4e3a346fcd233b91f2722d6d7325e50802afe481
穷屌丝 <mingkang1993@qq.com>
2017-10-13 03:38:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict'
 
-require('react-native/packager/babelRegisterOnly')([
-    /private-cli\/src/,
-    /local-cli/,
-])
+// require('react-native/packager/babelRegisterOnly')([
+//     /private-cli\/src/,
+//     /local-cli/,
+// ])
 require('react-native/setupBabel')();
-var Bundle = require('react-native/packager/src/Bundler/Bundle.js');
 var debug = require('react-native/local-cli/util/log').out('bundle');
 var path = require('path');
-var ReactPackager = require('react-native/packager/react-packager');
 var saveAssets = require('react-native/local-cli/bundle/saveAssets');
 var CacheBundle = require('./cacheBundle.js');
-var Server = require('react-native/packager/src/Server');
-var outputBundle = require('react-native/local-cli/bundle/output/bundle');
+var Server = require('metro-bundler/src/Server');
+var outputBundle = require('metro-bundler/src/shared/output/bundle');
+const TransformCaching = require('metro-bundler/src/lib/TransformCaching');
+const ConfigT = require('react-native/local-cli/util/Config');
+const Terminal = require('metro-bundler/src/lib/Terminal');
+const TerminalReporter = require('metro-bundler/src/lib/TerminalReporter');
 
 function createCodeWithMap(bundle, dev) {
     return {
@@ -20,84 +22,6 @@ function createCodeWithMap(bundle, dev) {
         map: JSON.stringify(bundle.getSourceMap({dev})),
     };
 }
-
-
-
-//function build(entryFile,args, config, bundleOptions){
-//    const cwd = args.cwd || process.cwd();
-//
-//    return ReactPackager.buildBundle({
-//        projectRoots: config.projectRoots,
-//        blacklistRE: config.blacklistRE,
-//        transformModulePath: args.transformer,
-//        verbose: args.verbose,
-//        //projectRoots: config.getProjectRoots(),
-//        //assetRoots: config.getAssetRoots(),
-//        //blacklistRE: config.getBlacklistRE(),
-//        //getTransformOptionsModulePath: config.getTransformOptionsModulePath,
-//      },{
-//        entryFile: path.resolve(cwd,entryFile),
-//        sourceMapUrl: args['sourcemap-output'],
-//        dev: args.dev,
-//        minify: !args.dev,
-//        platform: args.platform,
-//      }).then(function (c) {
-//        debug('Created ReactPackager');
-//        return c
-//      }).then(function (bundles) {
-//        var a = bundles.getModules();
-//        var newBundle = new Bundle();
-//
-//        for(var i in a){
-//            a.forEach(function(module) {
-//                if(filter(module.sourcePath)){
-//                    newBundle._modules.push(module);
-//                }
-//            })
-//        }
-//        newBundle.finalize();
-//        return {
-//            bundle: [bundles],
-//            moduleBundle: [newBundle]
-//        }
-//      })
-//      .then(function (bundles) {
-//        debug('Final Bundles', bundles.length);
-//
-//        bundles.forEach(function (bundle) {
-//          var bopts = Object.create(args)
-//          bopts['bundle-output'] = bundleOptions.entries(bundle._entry);
-//          outputBundle.save(bundle, bopts, debug)
-//        })
-//
-//        return bundles
-//      })
-//      .then(function (bundles) {
-//        debug('Assets')
-//        return Promise.all(
-//          bundles.map(function (bundle) {
-//            return saveAssets(bundle.getAssets(), args.platform, args['assets-dest'])
-//          })
-//        )
-//      })
-//}
-
-
-//        outputBundle.build(packagerInstance, {
-//            entryFile: path.resolve(cwd,bundleOptions.entries[0]),
-//            dev: args.dev,
-//            minify: !args.dev,
-//            platform: args.platform
-//        }).then(bundles => {
-//                           var bopts = Object.create(args);
-//
-//                     debug('Final Bundles', bundles.length);
-//
-//           bopts['bundleOutput'] = bundleOptions['bundleOutput']('1111');
-//           console.log(bopts)
-//
-//           outputBundle.save(bundles, bopts, debug);
-
 
 const bundleOptions = {
     entryModuleOnly: false,
@@ -115,27 +39,25 @@ const bundleOptions = {
 };
 
 
-
 module.exports = function (args, config, bundleOptions) {
     var cwd = args.cwd || process.cwd();
     debug('start');
+    const terminal = new Terminal(process.stdout);
 
-    const packagerInstance = new Server({
+    const packagerInstance = new Server(Object.assign({},ConfigT.DEFAULTS,{
+        transformCache: TransformCaching.useTempDir(),
         projectRoots: config.projectRoots,
         blacklistRE: config.blacklistRE,
         transformModulePath: args.transformer,
-        reporter:{
-            update:function(a){
-                console.log(a)
-            }
-        }
-    });
+        reporter: new TerminalReporter(terminal)
+    }));
 
-    const getClient = packagerInstance.buildBundle(Object.assign(bundleOptions,{
+    const getClient = packagerInstance.buildBundle(Object.assign({},Server.DEFAULT_BUNDLE_OPTIONS,{
         entryFile: bundleOptions.entries,
         dev: args.dev,
         minify: !args.dev,
-        platform: args.platform
+        platform: args.platform,
+        isolateModuleIDs: true
     })).then(function (bundles) {
         debug('start Bundles');
         const getModules = bundles.getModules();
@@ -195,103 +117,15 @@ module.exports = function (args, config, bundleOptions) {
             allOutputBundles: Promise.all(outputBundles)
         }
     })
-        .then(function (mainBundles) {
+    .then(function (mainBundles) {
 
-            debug('saveAssets')
-            mainBundles.allOutputBundles.then(()=>{
-                saveAssets(mainBundles.bundles.getAssets(),args.platform,bundleOptions.assetsDest).then(()=>{
-                debug('Closing client')
-            process.exit();
+        debug('saveAssets')
+        mainBundles.allOutputBundles.then(()=>{
+            saveAssets(mainBundles.bundles.getAssets(),args.platform,bundleOptions.assetsDest).then(()=>{
+                    debug('Closing client')
+                process.exit();
+            })
         })
-        })
-        })
-
+    })
 }
-
-
-
-////  process.env.NODE_ENV = args.dev ? 'development' : 'production';
-//  var getClient = ReactPackager.getOrderedDependencyPaths({
-//    projectRoots: config.projectRoots,
-//    blacklistRE: config.blacklistRE,
-//    transformModulePath: args.transformer,
-//    verbose: args.verbose
-//    //projectRoots: config.getProjectRoots(),
-//    //assetRoots: config.getAssetRoots(),
-//    //blacklistRE: config.getBlacklistRE(),
-//    //getTransformOptionsModulePath: config.getTransformOptionsModulePath,
-//  },{
-//    entryFile: path.resolve(cwd,bundleOptions.entries[0]),
-//    dev: args.dev,
-//    minify: !args.dev,
-//    platform: args.platform,
-//  }).then(function (c) {
-////  console.log(c)
-//    debug('Created ReactPackager');
-//    return c
-//  }).then(function (bundles) {
-////    var a = bundles.getModules();
-//    var newBundle = new Bundle();
-//    var libsBundle = new Bundle();
-//
-//    var mapFilterPath = [],
-//        mapLibsFilterPath = [];
-//                console.log()
-//
-//    bundles.forEach(function(module) {
-//    console.log(module)
-//        config.projectRoots.filter((root)=>{
-//            console.log(module.startsWith(root))
-//        })
-//
-////        if(filter(module.sourcePath,bundleOptions.filterEntryFile)){
-////            if(mapFilterPath.indexOf(module.sourcePath) === -1){
-////                mapFilterPath.push(module.sourcePath);
-////                newBundle._modules.push(module);
-////            }
-////        }else{
-////            if(mapLibsFilterPath.indexOf(module.sourcePath) === -1){
-////                mapLibsFilterPath.push(module.sourcePath);
-////                libsBundle._modules.push(module);
-////            }
-////        }
-//    });
-//
-//    libsBundle.finalize();
-//    newBundle.finalize();
-//
-//    return {
-//        moduleBundle: [newBundle],
-//        bundle: [libsBundle]
-//    }
-//  })
-//  .then(function (bundles) {
-//    debug('Final Bundles', bundles.length);
-//
-//    for(let key in bundles){
-//        bundles[key].forEach(function (bundle) {
-//          var bopts = Object.create(args);
-//          bopts['bundle-output'] = bundleOptions[key + 'Output'](bundle._entry);
-//
-//          outputBundle.save(bundle, bopts, debug)
-//        })
-//    }
-//
-//    return bundles
-//  })
-//  .then(function (bundles) {
-////    debug('Assets')
-////    return Promise.all(
-////      bundles.moduleBundle.map(function (bundle) {
-////        return saveAssets(bundle.getAssets(), args.platform, args['assets-dest'])
-////      })
-////    )
-//  })
-//  .then(function () {
-//    debug('Closing client')
-////    getClient.then(function (client) {
-////      mapFilterPath = [];
-////      client.close();
-////    })
-//  })
 
